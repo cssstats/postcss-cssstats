3d54f9fd4c371b6fb34787d8e1b56f06b652faa0
John Otander <johnotander@gmail.com>
2015-05-06 15:13:22 -0600

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var postcssCssstats = require('..');
+
+describe('postcss-cssstats', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(postcssCssstats(), true);
+  });
+});
