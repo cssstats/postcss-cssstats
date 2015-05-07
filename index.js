71e06e104b8898dc595bfa22c5a881937d64729e
John Otander <johnotander@gmail.com>
2015-05-06 22:05:10 -0600

    Update cssstats version to the ast compatible version

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ module.exports = postcss.plugin('cssstats', function(options, callback) {
   }
 
   return function(css, postcssResult) {
-    // XXX TODO: cssstats should also be able to handle an AST
-    callback(cssstats(css.toString()));
+    callback(cssstats(css));
   };
 });
